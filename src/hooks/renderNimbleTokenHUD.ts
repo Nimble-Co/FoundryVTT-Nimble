6a6c557bd1c014ff1b7a9d22ce4e5e0cb33f1b10
Nekro Darkmoon <nekrodarkmoon@gmail.com>
2025-07-09 22:23:50 -0700

    Updates rendering of Token Hud.

--- src/hooks/renderNimbleTokenHUD.ts
+++ src/hooks/renderNimbleTokenHUD.ts
@@ -2,7 +2,7 @@ import { mount } from 'svelte';
 import NimbleTokenHUD from '../view/pixi/NimbleTokenHUD.svelte';
 
 export default function renderNimbleTokenHUD(HUD, html, token) {
-	const target = $(html).find('.status-effects')[0];
+	const target = html.querySelector('.palette, .status-effects');
 	if (!target) return;
 
 	target.innerHTML = '';
