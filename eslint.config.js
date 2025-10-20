d0be381e131a89eb71422c16b0edb1eaf0d012a6
Quest L <trevlar@questluminary.com>
2025-10-19 23:32:36 -0600

    chore:Use prettier and config linter for Svelte

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import svelte from 'eslint-plugin-svelte';
+import globals from 'globals';
+import svelteParser from 'svelte-eslint-parser';
+
+export default [
+	js.configs.recommended,
+	{
+		files: ['**/*.svelte'],
+		languageOptions: {
+			parser: svelteParser,
+			parserOptions: {
+				parser: tsparser,
+			},
+			globals: {
+				...globals.browser,
+				// Foundry VTT globals
+				game: 'readonly',
+				CONFIG: 'readonly',
+				foundry: 'readonly',
+				canvas: 'readonly',
+				ui: 'readonly',
+				Hooks: 'readonly',
+				Roll: 'readonly',
+				fromUuid: 'readonly',
+				sheet: 'readonly',
+				TextEditor: 'readonly',
+			},
+		},
+		plugins: {
+			svelte,
+			'@typescript-eslint': tseslint,
+		},
+		rules: {
+			...svelte.configs.recommended.rules,
+			...tseslint.configs.recommended.rules,
+			// Add your custom rules here
+			'no-unused-vars': 'off', // Turn off for Svelte files as it has false positives with reactive statements
+		},
+	},
+];
