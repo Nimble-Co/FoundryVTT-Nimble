8e74325982990fa6a629211f2e77f2ff0047ea91
Oscar Martin <oscar.martin@tietoevry.com>
2026-01-10 13:33:40 +0100

    fix: ensure combat tracker updates on state changes

--- src/view/ui/CombatTracker.svelte
+++ src/view/ui/CombatTracker.svelte
@@ -190,34 +190,34 @@
 	});
 </script>
 
-{#if currentCombat}
-	<section class="nimble-combat-tracker" transition:slide={{ axis: 'x' }}>
-		<header
-			class="nimble-combat-tracker__header"
-			class:nimble-combat-tracker__header--no-controls={!game.user!.isGM}
-			class:nimble-combat-tracker__header--not-started={currentCombat?.round === 0}
-			in:slide={{ axis: 'y', delay: 200 }}
-			out:fade={{ delay: 0 }}
-		>
-			{#if currentCombat?.round === 0 && game.user!.isGM}
-				<button class="nimble-combat-tracker__start-button" onclick={startCombat}>
-					Start Combat
-				</button>
-			{:else if currentCombat?.round === 0}
-				<h2 class="nimble-combat-tracker__heading">Combat Not Started</h2>
-			{:else}
-				<h2 class="nimble-combat-tracker__heading">
-					Round {currentCombat?.round}
-				</h2>
-			{/if}
+{#key version}
+	{#if currentCombat}
+		<section class="nimble-combat-tracker" transition:slide={{ axis: 'x' }}>
+			<header
+				class="nimble-combat-tracker__header"
+				class:nimble-combat-tracker__header--no-controls={!game.user!.isGM}
+				class:nimble-combat-tracker__header--not-started={currentCombat?.round === 0}
+				in:slide={{ axis: 'y', delay: 200 }}
+				out:fade={{ delay: 0 }}
+			>
+				{#if currentCombat?.round === 0 && game.user!.isGM}
+					<button class="nimble-combat-tracker__start-button" onclick={startCombat}>
+						Start Combat
+					</button>
+				{:else if currentCombat?.round === 0}
+					<h2 class="nimble-combat-tracker__heading">Combat Not Started</h2>
+				{:else}
+					<h2 class="nimble-combat-tracker__heading">
+						Round {currentCombat?.round}
+					</h2>
+				{/if}
 
-			{#if currentCombat?.round !== 0 && game.user!.isGM}
-				<CombatTrackerControls />
-			{/if}
-		</header>
+				{#if currentCombat?.round !== 0 && game.user!.isGM}
+					<CombatTrackerControls />
+				{/if}
+			</header>
 
-		<ol class="nimble-combatants" ondrop={(event) => _onDrop(event)} out:fade={{ delay: 0 }}>
-			{#key version}
+			<ol class="nimble-combatants" ondrop={(event) => _onDrop(event)} out:fade={{ delay: 0 }}>
 				{#each sceneCombatants as combatant (combatant._id)}
 					{@const CombatantComponent = getCombatantComponent(combatant)}
 
@@ -228,24 +228,24 @@
 						/>
 					</li>
 				{/each}
-			{/key}
-		</ol>
+			</ol>
 
-		{#if game.user!.isGM && sceneCombatants.some((combatant) => combatant.initiative === null)}
-			<footer class="nimble-combat-tracker__footer">
-				<div class="nimble-combat-tracker__footer-roll-container">
-					<button
-						class="nimble-combat-tracker__footer-button"
-						type="button"
-						aria-label="Roll All"
-						data-tooltip="Roll All"
-						data-tooltip-direction="UP"
-						onclick={(event) => rollInitiativeForAll(event)}
-					>
-						<i class="fa-solid fa-users"></i>
-					</button>
-				</div>
-			</footer>
-		{/if}
-	</section>
-{/if}
+			{#if game.user!.isGM && sceneCombatants.some((combatant) => combatant.initiative === null)}
+				<footer class="nimble-combat-tracker__footer">
+					<div class="nimble-combat-tracker__footer-roll-container">
+						<button
+							class="nimble-combat-tracker__footer-button"
+							type="button"
+							aria-label="Roll All"
+							data-tooltip="Roll All"
+							data-tooltip-direction="UP"
+							onclick={(event) => rollInitiativeForAll(event)}
+						>
+							<i class="fa-solid fa-users"></i>
+						</button>
+					</div>
+				</footer>
+			{/if}
+		</section>
+	{/if}
+{/key}
