fe00352fc509ac99d619b67853e0bd94ed16f1c5
7D7D <paulthiriot@gmail.com>
2026-02-09 23:03:29 -0700

    Added Subclass Folders

--- build/lib/LevelDB.mjs
+++ build/lib/LevelDB.mjs
@@ -1,104 +1,118 @@
 import { ClassicLevel } from 'classic-level';
 import path from 'path';
-import systemJSON from '../../public/system.json' with { type: 'json'};
+import systemJSON from '../../public/system.json' with { type: 'json' };
 
 export default class LevelDatabase extends ClassicLevel {
-  #dbKey;
+	#dbKey;
 
-  #embeddedKeys;
+	#embeddedKeys;
 
-  #documentDb;
+	#documentDb;
 
-  #embeddedDbs;
+	#embeddedDbs;
 
-  constructor(location, options) {
-    const dbOptions = options.dbOptions ?? { keyEncoding: 'utf8', valueEncoding: 'json' };
-    super(location, dbOptions);
+	constructor(location, options) {
+		const dbOptions = options.dbOptions ?? { keyEncoding: 'utf8', valueEncoding: 'json' };
+		super(location, dbOptions);
 
-    const { dbKey, embeddedKeys } = this.#getDBKeys(options.packName);
+		const { dbKey, embeddedKeys } = this.#getDBKeys(options.packName);
 
-    this.dbOptions = dbOptions;
-    this.#dbKey = dbKey;
-    this.#embeddedKeys = embeddedKeys ?? [];
+		this.dbOptions = dbOptions;
+		this.#dbKey = dbKey;
+		this.#embeddedKeys = embeddedKeys ?? [];
 
-    this.#documentDb = this.sublevel(dbKey, dbOptions);
+		this.#documentDb = this.sublevel(dbKey, dbOptions);
 
-    if (this.#embeddedKeys.length) {
-      this.#embeddedDbs = this.#embeddedKeys.map((key) => ({
-        key: key.replaceAll('.', '-'),
-        db: this.sublevel(`${this.#dbKey}.${key}`, dbOptions)
-      }));
-    }
-  }
+		if (this.#embeddedKeys.length) {
+			this.#embeddedDbs = this.#embeddedKeys.map((key) => ({
+				key: key.replaceAll('.', '-'),
+				db: this.sublevel(`${this.#dbKey}.${key}`, dbOptions),
+			}));
+		}
+	}
 
-  #getDBKeys(packName) {
-    const metadata = systemJSON.packs
-      .find((p) => path.basename(p.path).split('.')[0] === packName);
+	#getDBKeys(packName) {
+		const metadata = systemJSON.packs.find((p) => path.basename(p.path).split('.')[0] === packName);
 
-    if (!metadata) throw Error(`[ERROR] - Pack ${packName} isn't setup in system.json.`);
+		if (!metadata) throw Error(`[ERROR] - Pack ${packName} isn't setup in system.json.`);
 
-    let dbKey = null;
-    if (metadata.type === 'JournalEntry') dbKey = 'journal';
-    else if (metadata.type === 'RollTable') dbKey = 'tables';
-    else dbKey = `${metadata.type.toLowerCase()}s`;
+		let dbKey = null;
+		if (metadata.type === 'JournalEntry') dbKey = 'journal';
+		else if (metadata.type === 'RollTable') dbKey = 'tables';
+		else dbKey = `${metadata.type.toLowerCase()}s`;
 
-    let embeddedKeys = [];
-    if (dbKey === 'actors') embeddedKeys = ['effects', 'items', 'items.effects'];
-    if (dbKey === 'items') embeddedKeys = ['effects'];
-    else if (dbKey === 'journal') embeddedKeys = ['pages'];
-    else if (dbKey === 'tables') embeddedKeys = ['results'];
+		let embeddedKeys = [];
+		if (dbKey === 'actors') embeddedKeys = ['effects', 'items', 'items.effects'];
+		if (dbKey === 'items') embeddedKeys = ['effects'];
+		else if (dbKey === 'journal') embeddedKeys = ['pages'];
+		else if (dbKey === 'tables') embeddedKeys = ['results'];
 
-    return { dbKey, embeddedKeys };
-  }
+		return { dbKey, embeddedKeys };
+	}
 
-  async createPack(docs) {
-    const docBatch = this.#documentDb.batch();
-    const embeddedBatches = this.#embeddedDbs.reduce((acc, { key, db }) => {
-      acc[key] = db.batch();
-      return acc;
-    }, {});
+	async createPack(docs, options = {}) {
+		const folders = Array.isArray(options.folders) ? options.folders : [];
 
-    for (const source of docs) {
-      if (this.#embeddedKeys.length) {
-        this.#embeddedKeys.forEach((key) => {
-          if (key === 'items.effects') return; // TODO:: Make this generic
-          if (this.#dbKey === 'actors' && key === 'items') {
-            const items = source[key];
+		const docBatch = this.#documentDb.batch();
+		const embeddedBatches = this.#embeddedDbs.reduce((acc, { key, db }) => {
+			acc[key] = db.batch();
+			return acc;
+		}, {});
+		const folderDb = folders.length > 0 ? this.sublevel('folders', this.dbOptions) : null;
+		const folderBatch = folderDb ? folderDb.batch() : null;
 
-            // Do effects
-            for (const item of items) {
-              const { effects } = item;
-              this.#addDataToBatch(effects, embeddedBatches['items-effects'], `${source._id}.${item._id}`);
-            }
+		for (const source of docs) {
+			if (this.#embeddedKeys.length) {
+				this.#embeddedKeys.forEach((key) => {
+					if (key === 'items.effects') return; // TODO:: Make this generic
+					if (this.#dbKey === 'actors' && key === 'items') {
+						const items = source[key];
 
-            // Do items
-            this.#addDataToBatch(items, embeddedBatches[key], source._id);
-          } else {
-            const embeddedDocs = source[key];
-            this.#addDataToBatch(embeddedDocs, embeddedBatches[key], source._id);
-          }
-        });
-      }
-      docBatch.put(source._id ?? '', source);
-    }
+						// Do effects
+						for (const item of items) {
+							const { effects } = item;
+							this.#addDataToBatch(
+								effects,
+								embeddedBatches['items-effects'],
+								`${source._id}.${item._id}`,
+							);
+						}
 
-    await docBatch.write();
-    for await (const batch of Object.values(embeddedBatches)) {
-      if (batch.length) await batch.write();
-    }
+						// Do items
+						this.#addDataToBatch(items, embeddedBatches[key], source._id);
+					} else {
+						const embeddedDocs = source[key];
+						this.#addDataToBatch(embeddedDocs, embeddedBatches[key], source._id);
+					}
+				});
+			}
+			docBatch.put(source._id ?? '', source);
+		}
 
-    await this.close();
-  }
+		if (folderBatch) {
+			for (const folder of folders) {
+				folderBatch.put(folder._id ?? '', folder);
+			}
+		}
 
-  #addDataToBatch(embeddedDocs, batch, sourceId) {
-    if (Array.isArray(embeddedDocs)) {
-      for (let i = 0; i < embeddedDocs.length; i += 1) {
-        const doc = embeddedDocs[i];
-        if (batch) {
-          batch.put(`${sourceId}.${doc._id}`, doc);
-          embeddedDocs[i] = doc._id ?? '';
-        }
-      }
-    }
-  }
+		await docBatch.write();
+		for await (const batch of Object.values(embeddedBatches)) {
+			if (batch.length) await batch.write();
+		}
+		if (folderBatch?.length) await folderBatch.write();
+
+		await this.close();
+	}
+
+	#addDataToBatch(embeddedDocs, batch, sourceId) {
+		if (Array.isArray(embeddedDocs)) {
+			for (let i = 0; i < embeddedDocs.length; i += 1) {
+				const doc = embeddedDocs[i];
+				if (batch) {
+					batch.put(`${sourceId}.${doc._id}`, doc);
+					embeddedDocs[i] = doc._id ?? '';
+				}
+			}
+		}
+	}
 }
