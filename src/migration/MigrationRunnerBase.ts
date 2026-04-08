9a4caf05769c49b99bb6ab59b8b61c5b1df832a9
Quest L <trevlar@questluminary.com>
2026-04-08 12:30:41 -0600

    feat(migration): add Migration014 to populate granted feature IDs

--- src/migration/MigrationRunnerBase.ts
+++ src/migration/MigrationRunnerBase.ts
@@ -9,7 +9,7 @@ interface CollectionDiff<T = any> {
 class MigrationRunnerBase {
 	migrations: MigrationBase[];
 
-	static LATEST_SCHEMA_VERSION = 13;
+	static LATEST_SCHEMA_VERSION = 14;
 
 	static RECOMMENDED_SAFE_VERSION = 0;
 
