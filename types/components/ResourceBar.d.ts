e4e1a816bdbaba66e734385a131df540ed7a5dd6
fronix <fronix@fronix.se>
2026-09-01 16:05:31 +0200

    refactor(sheet): drop the resource bar's uncalled props

--- types/components/ResourceBar.d.ts
+++ types/components/ResourceBar.d.ts
@@ -1,21 +1,8 @@
-interface BaseProps {
+export interface ResourceBarProps {
 	current: number;
 	max: number;
-	compact?: boolean;
 	disableControls?: boolean;
 	disableMaxEdit?: boolean;
 	updateCurrent?: (value: number) => void;
 	updateMax?: (value: number) => void;
 }
-
-interface WithoutControls extends BaseProps {
-	disableControls: true;
-}
-
-interface WithControls extends BaseProps {
-	disableControls?: false;
-	updateCurrent: NonNullable<BaseProps['updateCurrent']>;
-	updateMax?: BaseProps['updateMax'];
-}
-
-export type ResourceBarProps = WithControls | WithoutControls;
